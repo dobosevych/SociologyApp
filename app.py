9e0a802b2711f1707ff88524e0b942869ffceb2a
dobosevych <dobosevych@gmail.com>
2017-01-24 15:05:24 +0200

    Few changes

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, session, request, redirect
+from flask import Flask, render_template, session, request, redirect, flash
+from flask_wtf.csrf import CsrfProtect
+
 import os
 import json
+from forms import OpenForm
+
 
 app = Flask(__name__)
 app.secret_key = 'f04c8f9e2328040b2716dd6a294fc122b8c478b524688dbd'
+CsrfProtect(app)
+
+path_to_questions = os.path.join(os.path.dirname(__file__), 'questions/uk.json')
+with open(path_to_questions, encoding='UTF-8') as data:
+    questions = json.load(data)
+
+translation = questions['translation']
+questions = questions['questions']
+
+
 
 @app.route('/')
 def index():
@@ -17,7 +31,6 @@ def poll():
     if request.method == 'POST':
         if 'lang' in request.form:
             session['lang'] = request.form['lang']
-
         if 'question-id' in request.form:
             session['question-id'] = request.form['question-id']
         else:
@@ -48,6 +61,28 @@ def poll():
     else:
         print(question['type'])
         return 'Error 404'
+
+@app.route('/new-poll', methods=['GET', 'POST'])
+def new_poll():
+    if 'q_num' not in session:
+        session['q_num'] = 1
+
+    question = questions[session['q_num']]
+    form = OpenForm(request.form)
+
+    if question['type'] == 'open':
+        form.question.label = question['text']
+
+    if request.method == 'POST' and form.validate():
+        form.reset()
+        session['q_num'] += 1
+        question = questions[session['q_num']]
+
+        if question['type'] == 'open':
+            form.question.label = question['text']
+
+    q_num = session['q_num']
+    return render_template('open-v2.html', form=form, question=question, q_num=q_num, translation=translation)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
